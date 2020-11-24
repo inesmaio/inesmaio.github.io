0d5a6c0cbdd3ab361dea39af03fb386e6db0fed2
inesmaio <inesmaio4@gmail.com>
2020-11-24 16:01:30 +0000

    bug: added mailto

--- components/header/header.component.jsx
+++ components/header/header.component.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <HeaderContent>
                 <Title>Inês Maio</Title>
                 <Icons>
-                    <a href=""><img src="/icons/email.svg" /></a>
+                    <a href="mailto:inesmaio4@gmail.com"><img src="/icons/email.svg" /></a>
                     <a href="https://twitter.com/inesmaio" target="_blank"><img src="/icons/twitter.svg" /></a>
                     <a href="https://github.com/inesmaio/" target="_blank"><img src="/icons/github.svg" /></a>
                     <a href="https://www.linkedin.com/in/inesmaio/" target="_blank"><img src="/icons/linkedin.svg" /></a>
